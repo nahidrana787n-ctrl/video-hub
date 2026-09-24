5be4092003587e62766dcfd0478e5a39eb32513a
nahidrana787n-ctrl <nahidrana787n@gmail.com>
2026-09-24 08:55:20 +0600

    Update verifyIdToken.js

--- deshipay-worker/src/verifyIdToken.js
+++ deshipay-worker/src/verifyIdToken.js
@@ -1,7 +1,7 @@
 import { decodeProtectedHeader, importX509, jwtVerify } from "jose";
 
 const CERTS_URL =
-  "https://www.googleapis.com/robot/v1/metadata/x509/[email protected]";
+  "https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com";
 
 let certCache = { certs: null, exp: 0 };
 
